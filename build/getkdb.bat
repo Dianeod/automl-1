e7ee9a563db51598b072036bd1e4c22eb71fe7a6
Dianeod <dodonoghue@kx.com>
2020-09-16 10:58:40 +0100

    show dir

--- build/getkdb.bat
+++ build/getkdb.bat
@@ -3,6 +3,8 @@ mkdir q
 copy -r %conda info --base%/q q 
 echo|set /P =%QLIC_KC% >q\kc.lic.enc
 certutil -decode q\kc.lic.enc q\kc.lic
+dir q
+dir q/w64
 set QHOME=%cd%\q
 set PATH=%QHOME%\w64;%PATH%
 echo show .z.K;show .z.k;exit 0 | q -q
