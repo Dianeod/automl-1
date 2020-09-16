eb221aef711cd94212f8da54355c72953978d424
Dianeod <dodonoghue@kx.com>
2020-09-16 11:39:57 +0100

    upgraded matlotlib

--- build/test.bat
+++ build/test.bat
@@ -2,7 +2,9 @@ if defined QLIC_KC (
         pip -q install -r requirements.txt
 	echo getting test.q from embedpy
         git clone https://github.com/KxSystems/ml.git
+	git clone https://github.com/KxSystems/nlp.git
 	pip -q install -r nlp/requirements.txt
+	pip install --upgrade matplotlib==3.2.1
 	pip install gensim
 	pip install sobol-seq
         curl -fsSL -o test.q https://github.com/KxSystems/embedpy/raw/master/test.q
