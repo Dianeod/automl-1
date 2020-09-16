d247df462b13c496659c8dfac4aac1fd575d460f
Dianeod <dodonoghue@kx.com>
2020-09-16 12:01:45 +0100

    removed matplotlib

--- build/test.bat
+++ build/test.bat
@@ -4,7 +4,6 @@ if defined QLIC_KC (
         git clone https://github.com/KxSystems/ml.git
 	git clone https://github.com/KxSystems/nlp.git
 	pip -q install -r nlp/requirements.txt
-	pip install --upgrade matplotlib==3.2.1
 	pip install gensim
 	pip install sobol-seq
         curl -fsSL -o test.q https://github.com/KxSystems/embedpy/raw/master/test.q
