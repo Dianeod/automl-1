163e120d4505c84daa6d2953ee288e6f27dea0d1
Dianeod <dodonoghue@kx.com>
2020-09-16 11:09:05 +0100

    /e

--- build/getkdb.bat
+++ build/getkdb.bat
@@ -1,7 +1,6 @@
 conda install -c kx embedPy
 mkdir q
-dir C:\Miniconda3-x64\q
-xcopy C:\Miniconda3-x64\q q
+xcopy /E C:\Miniconda3-x64\q q
 echo|set /P =%QLIC_KC% >q\kc.lic.enc
 certutil -decode q\kc.lic.enc q\kc.lic
 set QHOME=%cd%\q
