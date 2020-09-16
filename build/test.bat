1f0365a446628323ada3a3455e948f0c5ef42f6f
Dianeod <dodonoghue@kx.com>
2020-09-16 11:35:21 +0100

    removed nlp

--- build/test.bat
+++ build/test.bat
@@ -2,7 +2,6 @@ if defined QLIC_KC (
         pip -q install -r requirements.txt
 	echo getting test.q from embedpy
         git clone https://github.com/KxSystems/ml.git
-	git clone https://github.com/KxSystems/nlp.git
 	pip -q install -r nlp/requirements.txt
 	pip install gensim
 	pip install sobol-seq
