46e6f2caa7dee8ac7dc4f2050414d8501a2fb80d
Dianeod <dodonoghue@kx.com>
2020-09-16 11:00:49 +0100

    show conda base

--- build/getkdb.bat
+++ build/getkdb.bat
@@ -1,10 +1,9 @@
 conda install -c kx embedPy
 mkdir q
-copy -r %conda info --base%/q q 
+copy -r %conda info --base%/q q
+conda info --base
 echo|set /P =%QLIC_KC% >q\kc.lic.enc
 certutil -decode q\kc.lic.enc q\kc.lic
-dir q
-dir q/w64
 set QHOME=%cd%\q
 set PATH=%QHOME%\w64;%PATH%
 echo show .z.K;show .z.k;exit 0 | q -q
