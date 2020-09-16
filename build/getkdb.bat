d9b509e326c710cdeb67355ca27542a0fcaddb33
Dianeod <dodonoghue@kx.com>
2020-09-16 10:35:52 +0100

    conda install kdb

--- build/getkdb.bat
+++ build/getkdb.bat
@@ -1,6 +1,5 @@
-certutil -decode %W64% w64.zip
-mkdir q
-7z x w64.zip -oq
+conda install -c kx embedPy
+cp -r $(conda info --base)/q q 
 echo|set /P =%QLIC_KC% >q\kc.lic.enc
 certutil -decode q\kc.lic.enc q\kc.lic
 set QHOME=%cd%\q
